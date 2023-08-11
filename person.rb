b555057a27ce7b80e4dc646b641582ba1d6ce562
devalibello <alibelloimoukhuede@gmail.com>
2023-08-11 22:38:02 +0100

    Add associations

--- person.rb
+++ person.rb
@@ -10,12 +10,18 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @id = Random.rand(1..1000)
+    @rentals = []
   end
 
   def can_use_services?
     return true if @age >= 18 || @parent_permission == true
 
     false
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   def correct_name
